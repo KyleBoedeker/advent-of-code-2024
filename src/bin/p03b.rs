5769f148b3dae0936ba242ba3fb8db08a1174c40
Kyle Boedeker <kyle.boedeker01@gmail.com>
2024-12-14 23:58:52 -0600

    Improve day03b solution with match instead of if conditions

--- src/bin/p03b.rs
+++ src/bin/p03b.rs
@@ -2,19 +2,26 @@ use regex::Regex;
 use std::io;
 
 fn main() {
+    // match either:
+    //   mul(x,y)
+    //     (where x and y could be 1-3 digits)
+    //   do()
+    //   don't()
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
 
-    let mut total = 0;
+    let mut total: u32 = 0;
     let mut instruction_enabled = true;
 
     let stdin = io::read_to_string(io::stdin()).unwrap();
     for m in re.captures_iter(&stdin) {
-        if m[0].starts_with("don't") {
-            instruction_enabled = false;
-        } else if m[0].starts_with("do") {
-            instruction_enabled = true;
-        } else if instruction_enabled {
-            total += m[1].parse::<u32>().unwrap() * m[2].parse::<u32>().unwrap();
+        match m[0].as_ref() {
+            "don't()" => instruction_enabled = false,
+            "do()" => instruction_enabled = true,
+            _ => {
+                if instruction_enabled {
+                    total += m[1].parse::<u32>().unwrap() * m[2].parse::<u32>().unwrap()
+                }
+            }
         }
     }
     println!("total is: {}", total);
