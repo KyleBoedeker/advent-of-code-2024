1203ca5ae564458c3ecaf2c171b8bbff8d165c49
Kyle Boedeker <kyle.boedeker01@gmail.com>
2024-12-18 00:50:00 -0600

    Change p05b solution to use sort_pages

--- src/bin/p05b.rs
+++ src/bin/p05b.rs
@@ -13,18 +13,16 @@ fn fits_ordering(pages: &Vec<i32>, rules: &Vec<(i32, i32)>) -> bool {
     return true;
 }
 
-// make a pass at sorting the vec (doesn't fix without calling it a bunch)
-fn fix_ordering(pages: &mut Vec<i32>, rules: &Vec<(i32, i32)>) {
-    for (r1, r2) in rules {
-        if let Some(idx1) = pages.iter().position(|v| v == r1) {
-            if let Some(idx2) = pages.iter().position(|v| v == r2) {
-                if idx1 > idx2 {
-                    let tmp = pages.remove(idx1);
-                    pages.insert(idx2, tmp);
-                }
-            }
+fn sort_pages(a: &i32, b: &i32, rules: &Vec<(i32, i32)>) -> std::cmp::Ordering {
+    for (r1, r2) in rules.iter() {
+        if a == r1 && b == r2 {
+            return std::cmp::Ordering::Greater;
+        }
+        if a == r2 && b == r1 {
+            return std::cmp::Ordering::Less;
         }
     }
+    return std::cmp::Ordering::Equal;
 }
 
 fn main() {
@@ -44,10 +42,7 @@ fn main() {
     while let Some(Ok(line)) = lines.next() {
         let mut pages: Vec<i32> = line.split(',').map(|x| x.parse::<i32>().unwrap()).collect();
         if !fits_ordering(&pages, &ordering_rules) {
-            // ooga-booga a few times till the page is definitely sorted (I hate sorting)
-            for _ in 0..pages.len() {
-                fix_ordering(&mut pages, &ordering_rules);
-            }
+            pages.sort_by(|a, b| sort_pages(a, b, &ordering_rules));
             total += pages.iter().nth(pages.len() / 2).unwrap();
         }
     }
@@ -63,13 +58,13 @@ mod tests {
     fn ordering_works() {
         let rules = vec![(4, 5), (6, 7), (9, 10)];
         let pages = vec![9, 4, 5, 6, 10, 7];
+        // 4 and 5 are swapped
+        let mut pages_unsorted = vec![9, 5, 4, 6, 10, 7];
 
         assert!(fits_ordering(&pages, &rules));
-
-        // 4 and 5 are swapped
-        let rules = vec![(4, 5), (6, 7), (9, 10)];
-        let pages = vec![9, 5, 4, 6, 10, 7];
-
-        assert!(!fits_ordering(&pages, &rules));
+        assert!(!fits_ordering(&pages_unsorted, &rules));
+        pages_unsorted.sort_by(|a, b| sort_pages(a, b, &rules));
+        assert_eq!(pages_unsorted, pages);
+        assert!(fits_ordering(&pages_unsorted, &rules));
     }
 }
