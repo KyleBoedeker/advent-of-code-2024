061644eaaae1881bf3921943bfb1466b6fdf47ac
Kyle Boedeker <kyle.boedeker01@gmail.com>
2025-08-31 01:23:42 -0500

    Add massive speedup of 11b

--- src/bin/p11b.rs
+++ src/bin/p11b.rs
@@ -1,90 +1,29 @@
-use std::io::{self, BufRead};
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+    mem,
+    time::Instant,
+};
 
 use regex::Regex;
 
-struct Stone {
-    value: u64,
-    count: u64,
-    new_count: u64,
-    transition_values: (u64, Option<u64>),
-}
-
-impl Stone {
-    fn new(value: u64) -> Self {
-        Stone {
-            value,
-            count: 1,
-            new_count: 0,
-            transition_values: Stone::get_transition_values(value),
-        }
+fn get_transition_values(value: u64) -> (u64, Option<u64>) {
+    // rule 1: replace 0 with 1
+    if value == 0 {
+        return (1, None);
     }
-
-    fn get_transition_values(value: u64) -> (u64, Option<u64>) {
-        // rule 1: replace 0 with 1
-        if value == 0 {
-            return (1, None);
-        }
-        // rule 2: split stones in half with even # of digits
-        let stringy_stone = value.to_string();
-        if stringy_stone.len() % 2 == 0 {
-            let (left, right) = stringy_stone.split_at(stringy_stone.len() / 2);
-            return (left.parse().unwrap(), Some(right.parse().unwrap()));
-        }
-        // rule 3: multiply by 2024
-        (value * 2024, None)
+    // rule 2: split stones in half with even # of digits
+    let stringy_stone = value.to_string();
+    if stringy_stone.len() % 2 == 0 {
+        let (left, right) = stringy_stone.split_at(stringy_stone.len() / 2);
+        return (left.parse().unwrap(), Some(right.parse().unwrap()));
     }
-}
-
-struct StoneVec {
-    stones: Vec<Stone>,
-}
-
-impl StoneVec {
-    fn new() -> Self {
-        StoneVec { stones: Vec::new() }
-    }
-
-    fn from(stone_values: Vec<u64>) -> Self {
-        let mut sv = StoneVec::new();
-        for v in stone_values {
-            sv.stones.push(Stone::new(v));
-        }
-        sv
-    }
-
-    fn transition(&mut self) {
-        // value, count
-        let mut value_counts: Vec<(u64, u64)> = vec![];
-        for st in self.stones.iter() {
-            value_counts.push((st.transition_values.0, st.count));
-            if let Some(v1) = st.transition_values.1 {
-                value_counts.push((v1, st.count));
-            }
-        }
-        // add the missing stones
-        for (val, _count) in value_counts.iter() {
-            if !self.stones.iter().any(|s| s.value == *val) {
-                self.stones.push(Stone::new(*val));
-            }
-        }
-        // update new counts
-        for (val, count) in value_counts {
-            let st: &mut Stone = self.stones.iter_mut().find(|s| s.value == val).unwrap();
-            st.new_count += count;
-        }
-        // swap count out and reset new counts
-        for st in self.stones.iter_mut() {
-            st.count = st.new_count;
-            st.new_count = 0;
-        }
-    }
-
-    fn total_sum(&self) -> u64 {
-        self.stones.iter().map(|v| v.count).sum()
-    }
+    // rule 3: multiply by 2024
+    (value * 2024, None)
 }
 
 fn main() {
+    let start_instant = Instant::now();
     // read stdin
     let mut buffer = String::new();
     {
@@ -95,21 +34,66 @@ fn main() {
 
     // Parse stdin as a list of numbers
     let re = Regex::new(r"(\d+)").unwrap();
-    let stones: Vec<u64> = re
+    let input_stones: Vec<u64> = re
         .captures_iter(&buffer)
         .map(|v| v.get(0).unwrap().as_str().parse().unwrap())
         .collect();
 
-    let mut stone_vec = StoneVec::from(stones);
+    // next available stone index (used to index other arrays)
+    let mut next_stone_idx: usize = 0;
+    // lookup a stone's index by value (used to index other arrays)
+    let mut stone_idx_lookup: HashMap<u64, usize> = HashMap::default();
+    // Using fixed-size arrays since there _shouldn't_ be more than ~4k stones from my experiments
+    // stone counts is the pre-start-of-transition counts, stone_counts_new is used during the transition
+    let mut stone_counts = [0u64; 4096];
+    let mut stone_counts_new = [0u64; 4096];
+
+    // Add the initial set of stones and the count (1 by default) for each stone in the input
+    for s in input_stones {
+        let idx = stone_idx_lookup.entry(s).or_insert(next_stone_idx);
+        if *idx == next_stone_idx {
+            next_stone_idx += 1;
+        }
+        stone_counts[*idx] += 1;
+    }
 
     let num_blinks = 75;
     for _ in 0..num_blinks {
-        stone_vec.transition();
+        // get a snapshot of the stones we need to transition
+        let stones: Vec<u64> = stone_idx_lookup.keys().cloned().collect();
+
+        for stone_value in stones {
+            // could probably move this count outside the loop
+            let count = stone_counts[*stone_idx_lookup.get(&stone_value).unwrap()];
+
+            let (t1, t2) = get_transition_values(stone_value);
+
+            // get the index of the new stone and add the count of the transitioning stone
+            let idx = stone_idx_lookup.entry(t1).or_insert(next_stone_idx);
+            if *idx == next_stone_idx {
+                next_stone_idx += 1;
+            }
+            stone_counts_new[*idx] += count;
+
+            // ditto for t2
+            if let Some(t2) = t2 {
+                let idx = stone_idx_lookup.entry(t2).or_insert(next_stone_idx);
+                if *idx == next_stone_idx {
+                    next_stone_idx += 1;
+                }
+                stone_counts_new[*idx] += count;
+            }
+        }
+
+        // In with the new, out with the old
+        mem::swap(&mut stone_counts, &mut stone_counts_new);
+        stone_counts_new.iter_mut().for_each(|v| *v = 0);
     }
 
     println!(
-        "# of stones after blinking {} times: {}",
+        "# of stones after blinking {} times is {}. Took {:.3} seconds.",
         num_blinks,
-        stone_vec.total_sum()
+        stone_counts.iter().sum::<u64>(),
+        start_instant.elapsed().as_secs_f32(),
     );
 }
